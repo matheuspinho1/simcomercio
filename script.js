e8685d6b0a6dd09a7f1b1d58710194dda212bbaf
transformar-educacao <matheus.pinho@senac.br>
2024-11-14 10:37:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,31 @@ function toggleChat() {
 }
 
 function showSimuladorOperacao() {
+    const existingIframeContainer = document.getElementById('simulador-operacao-container');
+    if (existingIframeContainer) {
+        existingIframeContainer.remove();
+        document.removeEventListener('click', closeIframeOnClickOutside);
+        return;
+    }
+
     const iframeContainer = document.createElement('div');
     iframeContainer.id = 'simulador-operacao-container';
     iframeContainer.innerHTML = `
         <iframe src="https://copilotstudio.microsoft.com/environments/Default-978f5e39-48c9-42c9-80dd-415bdb8bca6c/bots/crd2d_simuladorDoVarejo2/webchat?__version__=2" frameborder="0"></iframe>
     `;
     document.body.appendChild(iframeContainer);
+
+    document.addEventListener('click', closeIframeOnClickOutside);
+
+    function closeIframeOnClickOutside(event) {
+        if (!iframeContainer.contains(event.target)) {
+            iframeContainer.remove();
+            document.removeEventListener('click', closeIframeOnClickOutside);
+        }
+    }
 }
 
-
-document.getElementById('simulador-operacao-button').addEventListener('click', showSimuladorOperacao);
+document.getElementById('simulador-operacao-button').addEventListener('click', function(event) {
+    event.stopPropagation();
+    showSimuladorOperacao();
+});
